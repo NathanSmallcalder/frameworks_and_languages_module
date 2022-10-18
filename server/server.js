32e536df1a5a449334355fd714d080235acc6982
Nathan Smallcalder <nsmallcalder5@gmail.com>
2022-10-18 15:29:16 +0000

    Added Cors Headers

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const cors = require('cors');
+const e = require('express');
 const app = express()
 const port = 8000
 
 app.use(express.json());
 
 storage = []
+var corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 app.get('/items', (req,res) => {
   res.json(storage)
@@ -12,24 +18,33 @@ app.get('/items', (req,res) => {
 })
 
 app.get('/', (req, res) => {
+  res.send("Hello world")
 })
 
 app.post('/item', (req, res) =>{
     console.log(storage)
     storage.push(req.body)
-    res.status(201).json(req.body)
-    
+    res.status(201).json()
   })
   
 app.delete('/item/:id', (req,res) => {
     const id = req.params.id;
     console.log(id)
     storage = [...storage.filter((storage)=>storage.id != id)]
-    console.log('deleted', storage)
-    res.status(204).json()
+    if(storage = '{}')
+    {
+      res.status(404).json()
+    }
+    else{
+      console.log('deleted', storage)
+      res.status(204).json()
+    }
 })
+
+app.options('*', cors())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  process.on('SIGINT', function() {process.exit()})
 })
+
+process.on('SIGINT', function() {process.exit()})
